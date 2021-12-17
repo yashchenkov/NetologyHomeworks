884a4fc9a12089c8a4bdbf8caf2226c6f7d4329c
Yashchenko Victor <Andrey@Komp11.local>
2021-12-17 18:33:09 +0300

    сделал дз по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if(arr1.length === arr2.length) {
+    result =   arr1.every((element, index) => arr2[index] === element );
+  } else {
+    result = false;
+  }
+  //console.log(result);
 
   return result; // boolean
 }
@@ -9,7 +14,9 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr
+    .filter(element => element > 0 && element % 3 === 0)
+    .map(elem => elem*10);
 
   return resultArr; // array
 }
