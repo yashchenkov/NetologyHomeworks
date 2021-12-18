8f17b42bf5553ebea66fa66cbda9c5c13e51b12b
Yashchenko Victor <Andrey@Komp11.local>
2021-12-18 23:03:56 +0300

    сделал д3 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,48 @@
 function Student(name, gender, age) {
-    // Ваш код
+    let marks;
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+    if(this.marks === undefined) {
+    this.marks = [];
+    mark.forEach( item => this.marks.push(item));
+  } else {
+    mark.forEach( item => this.marks.push(item));
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(item => sum += item);
+
+
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+
+}
+
+
 // ваш код для остальных методов
+let student1 = new Student('Buzz', 'male', 35);
